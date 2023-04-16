e6e89312ec536c987236411121932f4ed1bcfb23
AliciaBR02 <100451005@alumnos.uc3m.es>
2023-04-16 15:00:38 +0200

    corrigiendo scripts

--- run2.sh
+++ run2.sh
@@ -3,12 +3,7 @@
 #Colors
 declare -r greenColour="\e[0;32m\033[1m"
 declare -r endColour="\033[0m\e[0m"
-declare -r redColour="\e[0;31m\033[1m"
 declare -r blueColour="\e[0;34m\033[1m"
-declare -r yellowColour="\e[0;33m\033[1m"
-declare -r purpleColour="\e[0;35m\033[1m"
-declare -r turquoiseColour="\e[0;36m\033[1m"
-declare -r grayColour="\e[0;37m\033[1m"
 
 UNO="$(./runclnt1.sh &)"
 DOS="$(./runclnt2.sh &)"
